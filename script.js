7db9dcee6461d6768eadbc9d9c4d98422365d416
Noriel Dimaano <norieldimaano@gmail.com>
2023-09-29 23:25:13 +0800

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#input').change(function(){
+var input = $(this).val();
+$('ul').append('<li>' + input + '<i class="fas fa-check"></i>  <i class="fas fa-trash"></i></li>');
+$(this).val('');
+});
+
+$('ul').on('click','.fa-trash',function(){
+    $(this).parent('li').fadeOut();
+})
+$('ul').on('click','.fa-check',function(){
+    $(this).parent('li').toggleClass('checked');
+
+});
+
+});
+
